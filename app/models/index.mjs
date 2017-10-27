534447a914fcfdd35f8e17066f46de60e22a0fab
chibie <chibuotu@gmail.com>
2017-10-27 16:27:56 +0100

    create student model

---
+++ app/models/index.mjs
@@ -0,0 +1,6 @@
+import Student from './student';
+
+const models = {};
+models.Student = Student;
+
+export default models;
---
+++ app/models/student.mjs
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const LEVELS = ['junior', 'senior'];
+const GENDERS = ['male', 'female'];
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    first: {type: String, required: true},
+    last: {type: String, required: true},
+    others: String,
+  },
+  gender: {type: String, enum: GENDERS},
+  dateOfBirth: {type: Date, required: true},
+  photoUrl: {type: String, required: true},
+  class_: {type: Number, required: true, min: 1, max: 3},
+  level: {type: String, enum: LEVELS, required: true},
+  subjects: [String],
+},
+{// schema options
+  timestamps: true,
+});
+
+export default mongoose.model('Student', studentSchema);
