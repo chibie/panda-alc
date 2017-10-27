3973eac1d9b7657d34f60db0b1dbfccb7db043a6
chibie <chibuotu@gmail.com>
2017-10-27 16:35:52 +0100

    improve error handling

--- app/routes/student.mjs
+++ app/routes/student.mjs
@@ -1,7 +1,40 @@
+/* eslint-disable */
+
 import API from 'mongoose-restify';
 import models from '../models';
 
-const studentAPI = new API({
+class StudentAPI extends API {
+
+  onError(req, res, err) {
+    console.error(err.name + ': ' + err.message);
+
+    if (err.name === "ValidationError") {
+      return res.status(400).json({
+        code: 'BadRequest',
+        message: err.message,
+        status: 400,
+      });
+    }
+
+    if (err.name === "HttpError") {
+      return res.status(404).json({
+        code: 'NotFound',
+        message: 'Resource not found.',
+        status: 404,
+      });
+    }
+
+    return res.status(500).json({
+      code: 'InternalServerError',
+      message: err.message,
+      status: 500,
+    });
+  }
+};
+
+/* eslint-enable */
+
+const studentAPI = new StudentAPI({
   path: '/students',
   model: models.Student,
 });
